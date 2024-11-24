42cb33db354f8df6a18c9a5aa04f8dbb8d36a429
shu-vro <official.shirshen@gmail.com>
2024-11-25 02:22:25 +0600

    chore: add pwa support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+import withPWAInit from "@ducanh2912/next-pwa";
+
+// /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: false,
 };
 
-export default nextConfig;
+const withPWA = withPWAInit({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    // register: true,
+    // scope: "/app",
+    // sw: "service-worker.js",
+    //...
+});
+
+// Your Next config is automatically typed!
+export default withPWA(nextConfig);

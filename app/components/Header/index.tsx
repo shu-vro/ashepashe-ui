2f169c841c6e1305c8fe8fbd7bb6fd748d394f49
shu-vro <official.shirshen@gmail.com>
2024-12-01 01:54:09 +0600

    feat!: desktop sidebar removed.  desktop topbar added

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import {
     Navbar,
     NavbarBrand,
     NavbarContent,
     NavbarItem,
     Image,
+    Tabs,
+    Tab,
 } from "@nextui-org/react";
 import Link from "next/link";
 import { SidebarTrigger } from "@/components/ui/sidebar";
@@ -17,6 +19,8 @@ import { PiSidebarSimpleLight } from "react-icons/pi";
 import { API_URL } from "@/lib/var";
 import ResponsiveButtons from "./ResponsiveSearch";
 import SearchDesktop from "./SearchDesktop";
+import { Category, getSidebarItems } from "../AppSidebar";
+import { usePathname } from "next/navigation";
 
 async function getAllProducts() {
     const response = await fetch(`${API_URL}/products`);
@@ -24,11 +28,36 @@ async function getAllProducts() {
     return data;
 }
 
+type SidebarItem = { id: string; name: string };
+
 export default function Header() {
     const [products, setProducts] = React.useState<Product["product"][]>([]);
+    const [sidebarItems, setSidebarItems] = useState<SidebarItem[]>([
+        { id: "/", name: "Home" },
+        { id: "/all-companies", name: "All Companies" },
+        { id: "/all-products", name: "All Products" },
+    ]);
+    const pathName = usePathname();
 
     React.useEffect(() => {
+        // generate n random sidebarItems
+
         getAllProducts().then(setProducts).catch(console.error);
+        getSidebarItems()
+            .then((data) => {
+                let newData = data.map((item) => ({
+                    id: `/category/${item.name}`,
+                    name: item.name,
+                }));
+                setSidebarItems((p) => {
+                    // maybe ids in p are equal. if they are, return only unique items
+                    return [...p, ...newData].filter(
+                        (item, index, self) =>
+                            index === self.findIndex((t) => t.id === item.id)
+                    );
+                });
+            })
+            .catch(console.error);
     }, []);
 
     return (
@@ -67,7 +96,27 @@ export default function Header() {
                     <ResponsiveButtons products={products} />
                 </NavbarContent>
             </Navbar>
-            {/* <Navbar></Navbar> */}
+            <Navbar
+                isBordered
+                shouldHideOnScroll
+                maxWidth="full"
+                className="overflow-x-auto w-full max-md:hidden top-[5.5rem] opacity-1 data-[hidden=true]:opacity-0 data-[hidden=true]:pointer-events-none transition-all z-30">
+                <NavbarContent justify="center" className="mx-auto">
+                    <Tabs
+                        aria-label="navigation items"
+                        variant="underlined"
+                        items={sidebarItems}
+                        selectedKey={pathName}>
+                        {(item) => (
+                            <Tab
+                                key={item.id}
+                                title={item.name}
+                                as={Link}
+                                href={item.id as string}></Tab>
+                        )}
+                    </Tabs>
+                </NavbarContent>
+            </Navbar>
         </>
     );
 }

f68dffd72412552a13f19716ec3e38467b4a46d9
Evangel Puri <purificationevan04@gmail.com>
2025-03-21 17:25:03 +0600

    Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -122,11 +122,41 @@ export default function RootLayout({
                         );
                         fbq("init", "3276434292511264");
                         fbq("track", "PageView");
-                        fbq('track', 'CompleteRegistration');
                     `}
                 </Script>
+
+                {/* Google Tag Manager - head part */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              (function(w,d,s,l,i){
+                w[l] = w[l] || []; 
+                w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'}); 
+                var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:''; 
+                j.async=true; 
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl; 
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PMRZ564N');
+            `,
+                    }}
+                ></script>
+
+
             </head>
-            <body className={`${font.className} antialiased`}>{children}</body>
+            <body className={`${font.className} antialiased`}>
+
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-PMRZ564N"
+                        height="0"
+                        width="0"
+                        style="display:none;visibility:hidden">
+                    </iframe>
+                </noscript>
+
+
+                {children}
+            </body>
         </html>
     );
 }
